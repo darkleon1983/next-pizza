17b84b8296b7fb2c833395dd5e5d2ed6804a859e
unknown <mts9832899777@gmail.com>
2024-08-05 17:21:06 +0300

    made header, and decided to make React-pizza

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1>
-      Home
-      <Button variant="outline">Корзина</Button>
-    </h1>
-  );
+  return <h1>Главная</h1>;
 }
