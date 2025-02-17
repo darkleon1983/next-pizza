28d34c4bf3e7415cbb00136ddb1384c4547fa2bc
evgeny korolev <mts9832899777@gmail.com>
2025-02-17 19:43:08 +0300

    added pizza categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Container, Title, Categories, SortPopup } from "@/components/shared";
 
 export default function Home() {
-  return <h1>Главная</h1>;
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+        <Categories />
+        <SortPopup />
+      </Container>
+      <h1>Главная</h1>;
+    </>
+  );
 }
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -14,6 +14,7 @@ export const Header: React.FC<Props> = ({ className }) => {
     <header className={cn("border border-b", className)}>
       <Container className="flex items-center justify-between py-8">
         <div className="flex items-center gap-4">
+          {/*Левая часть */}
           <Image src="/logo.png" alt="Logo" width={35} height={35} />
           <div>
             <h1 className="text-2xl uppercase font-black">Next pizza</h1>
@@ -22,6 +23,7 @@ export const Header: React.FC<Props> = ({ className }) => {
             </p>
           </div>
         </div>
+        {/*Правая часть*/}
         <div className="flex items-center gap-3">
           <Button variant="outline" className="flex items-center gap-1">
             <User size={16} />
@@ -32,10 +34,13 @@ export const Header: React.FC<Props> = ({ className }) => {
               <b>520 Р</b>
               <span className="h-full w-[1px] bg-white/30 mx-3" />
               <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-5">
-                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+                <ShoppingCart size={16} className="relative" strokeWidth={2} />
                 <b>3</b>
               </div>
-              <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+              <ArrowRight
+                size={20}
+                className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+              />
             </Button>
           </div>
         </div>
